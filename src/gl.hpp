9f5ecb95df244527d4c246b8a4355dafa667a402
Stephan <xuzihanapple@live.com>
2021-05-21 19:18:50 +0800

    Fix problems with HiDPI, Add serialport

--- src/gl.hpp
+++ src/gl.hpp
@@ -1,5 +1,6 @@
 #include <imgui.h>
 #include <imgui_stdlib.h>
+#include <imgui_freetype.h>
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
 #include <implot.h>
--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include "gl.hpp"
 #include "series.hpp"
 #include "chart.hpp"
+#include "serial.hpp"
 
 #ifdef _WIN32
 #define NOMINMAX
@@ -17,6 +18,26 @@
 #endif // _WIN32
 
 CMRC_DECLARE(resources);
+
+Serial serial;
+std::string deviceName;
+int baudRate = 0;
+int characterSize = 0;
+const char *stopBitItems[] = {u8"1", u8"1.5", u8"2"};
+int curtStopBit = 0;
+const char *parityItems[] = {u8"无校验", u8"奇校验", u8"偶校验"};
+int curtParity = 0;
+const char *flowControlItems[] = {u8"无", u8"软件", u8"硬件"};
+int curtFlowControl = 0;
+
+const char *pidModeItems[] = {u8"位置式"};
+int curtPidMode = 0;
+int motorId = 0;
+float pidOutMax = 0;
+float controlMatrix[3][3] = {};
+
+float chartTimeLimit = 5000.f;
+std::string connectErrorTips;
 
 static void error_callback(int error, const char *description) {
     fprintf(stderr, "Error: %s\n", description);
@@ -57,7 +78,7 @@ static void StyleColorsVisualStudio(ImGuiStyle *dst = nullptr) {
     const ImVec4 textColor = ColorFromBytes(255, 255, 255);
     const ImVec4 textDisabledColor = ColorFromBytes(151, 151, 151);
     const ImVec4 borderColor = ColorFromBytes(78, 78, 78);
-
+    style.WindowMenuButtonPosition = ImGuiDir_Right;
     colors[ImGuiCol_Text] = textColor;
     colors[ImGuiCol_TextDisabled] = textDisabledColor;
     colors[ImGuiCol_TextSelectedBg] = panelActiveColor;
@@ -115,53 +136,19 @@ static void StyleColorsVisualStudio(ImGuiStyle *dst = nullptr) {
     style.TabRounding = 0.0f;
 }
 
-static void StyleColorsDracula(ImGuiStyle *dst = nullptr) {
-    auto *style = (dst ? dst : &ImGui::GetStyle());
-    style->WindowRounding = 5.3f;
-    style->GrabRounding = style->FrameRounding = 2.3f;
-    style->ScrollbarRounding = 5.0f;
-    style->FrameBorderSize = 1.0f;
-    style->ItemSpacing.y = 6.5f;
-
-    style->WindowMenuButtonPosition = ImGuiDir_Right;
-    style->Colors[ImGuiCol_Text] = {0.73333335f, 0.73333335f, 0.73333335f, 1.00f};
-    style->Colors[ImGuiCol_TextDisabled] = {0.34509805f, 0.34509805f, 0.34509805f, 1.00f};
-    style->Colors[ImGuiCol_WindowBg] = {0.23529413f, 0.24705884f, 0.25490198f, 0.94f};
-    style->Colors[ImGuiCol_ChildBg] = {0.23529413f, 0.24705884f, 0.25490198f, 0.00f};
-    style->Colors[ImGuiCol_PopupBg] = {0.23529413f, 0.24705884f, 0.25490198f, 0.94f};
-    style->Colors[ImGuiCol_Border] = {0.33333334f, 0.33333334f, 0.33333334f, 0.50f};
-    style->Colors[ImGuiCol_BorderShadow] = {0.15686275f, 0.15686275f, 0.15686275f, 0.00f};
-    style->Colors[ImGuiCol_FrameBg] = {0.16862746f, 0.16862746f, 0.16862746f, 0.54f};
-    style->Colors[ImGuiCol_FrameBgHovered] = {0.453125f, 0.67578125f, 0.99609375f, 0.67f};
-    style->Colors[ImGuiCol_FrameBgActive] = {0.47058827f, 0.47058827f, 0.47058827f, 0.67f};
-    style->Colors[ImGuiCol_TitleBg] = {0.2392156863f, 0.2470588235f, 0.2549019608f, 1.00f};
-    style->Colors[ImGuiCol_TitleBgCollapsed] = {0.16f, 0.29f, 0.48f, 1.00f};
-    style->Colors[ImGuiCol_TitleBgActive] = ImVec4(0.23f, 0.28f, 0.33f, 0.51f);
-    style->Colors[ImGuiCol_MenuBarBg] = {0.27058825f, 0.28627452f, 0.2901961f, 0.80f};
-    style->Colors[ImGuiCol_ScrollbarBg] = {0.27058825f, 0.28627452f, 0.2901961f, 0.60f};
-    style->Colors[ImGuiCol_ScrollbarGrab] = ImVec4(0.39f, 0.40f, 0.40f, 0.51f);
-    style->Colors[ImGuiCol_ScrollbarGrabHovered] = {0.21960786f, 0.30980393f, 0.41960788f, 1.00f};
-    style->Colors[ImGuiCol_ScrollbarGrabActive] = {0.13725491f, 0.19215688f, 0.2627451f, 0.91f};
-    style->Colors[ImGuiCol_CheckMark] = {0.90f, 0.90f, 0.90f, 0.83f};
-    style->Colors[ImGuiCol_SliderGrab] = {0.70f, 0.70f, 0.70f, 0.62f};
-    style->Colors[ImGuiCol_SliderGrabActive] = {0.30f, 0.30f, 0.30f, 0.84f};
-    style->Colors[ImGuiCol_Button] = {0.33333334f, 0.3529412f, 0.36078432f, 0.49f};
-    style->Colors[ImGuiCol_ButtonHovered] = {0.21960786f, 0.30980393f, 0.41960788f, 1.00f};
-    style->Colors[ImGuiCol_ButtonActive] = {0.13725491f, 0.19215688f, 0.2627451f, 1.00f};
-    style->Colors[ImGuiCol_Header] = {0.33333334f, 0.3529412f, 0.36078432f, 0.53f};
-    style->Colors[ImGuiCol_HeaderHovered] = {0.453125f, 0.67578125f, 0.99609375f, 0.67f};
-    style->Colors[ImGuiCol_HeaderActive] = {0.47058827f, 0.47058827f, 0.47058827f, 0.67f};
-    style->Colors[ImGuiCol_Separator] = {0.31640625f, 0.31640625f, 0.31640625f, 1.00f};
-    style->Colors[ImGuiCol_SeparatorHovered] = {0.31640625f, 0.31640625f, 0.31640625f, 1.00f};
-    style->Colors[ImGuiCol_SeparatorActive] = {0.31640625f, 0.31640625f, 0.31640625f, 1.00f};
-    style->Colors[ImGuiCol_ResizeGrip] = {1.00f, 1.00f, 1.00f, 0.85f};
-    style->Colors[ImGuiCol_ResizeGripHovered] = {1.00f, 1.00f, 1.00f, 0.60f};
-    style->Colors[ImGuiCol_ResizeGripActive] = {1.00f, 1.00f, 1.00f, 0.90f};
-    style->Colors[ImGuiCol_PlotLines] = {0.61f, 0.61f, 0.61f, 1.00f};
-    style->Colors[ImGuiCol_PlotLinesHovered] = {1.00f, 0.43f, 0.35f, 1.00f};
-    style->Colors[ImGuiCol_PlotHistogram] = {0.90f, 0.70f, 0.00f, 1.00f};
-    style->Colors[ImGuiCol_PlotHistogramHovered] = {1.00f, 0.60f, 0.00f, 1.00f};
-    style->Colors[ImGuiCol_TextSelectedBg] = {0.18431373f, 0.39607847f, 0.79215693f, 0.90f};
+static void HandleSerialConnect() {
+    spdlog::info("Pressed");
+    serial.Connect(deviceName,
+                   baudRate,
+                   boost::asio::serial_port::stop_bits::type(curtStopBit),
+                   characterSize,
+                   boost::asio::serial_port::parity::type(curtParity),
+                   boost::asio::serial_port::flow_control::type(curtFlowControl));
+    if (!serial.IsValid()) {
+        connectErrorTips = u8"连接失败";
+        return;
+    }
+    serial.StartGrabbing();
 }
 
 int main() {
@@ -175,7 +162,7 @@ int main() {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    GLFWwindow *window = glfwCreateWindow(1280, 720, "BusPlot", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(1440, 900, "BusPlot", NULL, NULL);
     if (!window) {
         glfwTerminate();
         exit(EXIT_FAILURE);
@@ -196,16 +183,15 @@ int main() {
     ImGuiIO &io = ImGui::GetIO();
     io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
     io.IniFilename = nullptr;
-    ImGui::StyleColorsDark();
-    StyleColorsDracula();
+    StyleColorsVisualStudio();
+    ImGui::GetStyle().ScaleAllSizes(2.f);
 
     auto fs = cmrc::resources::get_filesystem();
     auto defaultLayoutContent = fs.open("asset/default_layout.ini");
     std::vector<char> defaultLayoutBuffer(defaultLayoutContent.begin(), defaultLayoutContent.end());
     ImGui::LoadIniSettingsFromMemory(defaultLayoutBuffer.data(), defaultLayoutBuffer.size());
-
     io.Fonts->AddFontFromFileTTF("asset/PingFang.ttc",
-                                 17.f,
+                                 18.f * 2.f,
                                  nullptr,
                                  io.Fonts->GetGlyphRangesChineseSimplifiedCommon());
     ImGui_ImplGlfw_InitForOpenGL(window, true);
@@ -224,25 +210,6 @@ int main() {
             std::this_thread::sleep_for(std::chrono::milliseconds(1));
         }
     }).detach();
-
-    const char *stopBitItems[] = {u8"1", u8"1.5", u8"2"};
-    int curtStopBit = 0;
-    const char *parityItems[] = {u8"无校验", u8"奇校验", u8"偶校验"};
-    int curtParity = 0;
-    const char *flowControlItems[] = {u8"无", u8"软件", u8"硬件"};
-    int curtFlowControl = 0;
-
-    const char *pidModeItems[] = {u8"位置式"};
-    int curtPidMode = 0;
-    int motorId = 0;
-    float pidOutMax = 0;
-    float controlMatrix[3][3] = {};
-
-    float chartTimeLimit = 5000.f;
-
-    std::string deviceConfig;
-    std::string baudrate;
-    std::string characterSize;
 
     while (!glfwWindowShouldClose(window)) {
         // Start the Dear ImGui frame
@@ -272,17 +239,23 @@ int main() {
             ImGui::End();
         }
 
-//        ImGui::ShowDemoWindow(nullptr);
-//        ImPlot::ShowDemoWindow(nullptr);
+        ImGui::ShowDemoWindow(nullptr);
+//        ImGui::ShowStyleEditor(nullptr);
+        ImPlot::ShowDemoWindow(nullptr);
 
         if (ImGui::Begin(u8"连接设置", nullptr)) {
-            ImGui::InputText(u8"设备名", &deviceConfig);
-            ImGui::InputText(u8"波特率", &baudrate);
+            ImGui::InputText(u8"设备名", &deviceName);
+            ImGui::InputInt(u8"波特率", &baudRate, 0);
             ImGui::Combo(u8"停止位", &curtStopBit, stopBitItems, sizeof(stopBitItems) / sizeof(const char *));
-            ImGui::InputText(u8"数据位", &characterSize);
+            ImGui::InputInt(u8"数据位", &characterSize, 0);
             ImGui::Combo(u8"奇偶校验", &curtParity, parityItems, sizeof(parityItems) / sizeof(const char *));
             ImGui::Combo(u8"流控制", &curtFlowControl, flowControlItems, sizeof(flowControlItems) / sizeof(const char *));
-            ImGui::Button(u8"连接", ImVec2(-1, 0));
+            if (ImGui::Button(u8"连接", ImVec2(-1, 0))) {
+                HandleSerialConnect();
+            }
+            if (connectErrorTips.length() > 0) {
+                ImGui::Text("%s", connectErrorTips.c_str());
+            }
             ImGui::End();
         }
 
@@ -303,17 +276,17 @@ int main() {
         if (ImGui::Begin(u8"参数设置", nullptr)) {
             ImGui::Combo(u8"模式", &curtPidMode, pidModeItems, sizeof(pidModeItems) / sizeof(const char *));
             ImGui::InputInt(u8"设备ID", &motorId);
-            ImGui::DragFloat3("Kp", controlMatrix[0]);
+            ImGui::DragFloat3(u8"Kp", controlMatrix[0]);
             ImGui::SameLine();
             HelpMarker(u8"设置P参数\n"
                        u8"拖动或双击修改参数值\n"
                        u8"值顺序为: (Scale, OutMax, Value)\n");
-            ImGui::DragFloat3("Ki", controlMatrix[1]);
+            ImGui::DragFloat3(u8"Ki", controlMatrix[1]);
             ImGui::SameLine();
             HelpMarker(u8"设置I参数\n"
                        u8"拖动或双击修改参数值\n"
                        u8"值顺序为: (Scale, OutMax, Value)\n");
-            ImGui::DragFloat3("Kd", controlMatrix[2]);
+            ImGui::DragFloat3(u8"Kd", controlMatrix[2]);
             ImGui::SameLine();
             HelpMarker(u8"设置D参数\n"
                        u8"拖动或双击修改参数值\n"
@@ -330,7 +303,6 @@ int main() {
         int width, height;
         glfwGetFramebufferSize(window, &width, &height);
         glViewport(0, 0, width, height);
-        glClearColor(0.f, 0.f, 0.f, 0.f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
